47f28a86fb259c1dac0557fdbd092e09702f6d8c
Denis Demin <77795430+prostodennik@users.noreply.github.com>
2024-01-21 16:55:18 +0300

    add: component InstallPWA

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -4,10 +4,12 @@ import About from '../About/About';
 import Price from '../Price/Price';
 import Photo from '../Photo/Photo';
 import Contacts from '../Contacts/Contacts';
+import InstallPWA from '../InstallPWA/InstallPWA';
 
 const MainPage = () => {
     return (
         <>
+            <InstallPWA />
             <Top />
             <About />
             <Price />
--- src/components/Stock/Stock.jsx
+++ src/components/Stock/Stock.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ImgStock from '../../assets/images/stock.jpg';
 import MobileMenu from '../MobileMenu/ModileMenu';
+import InstallPWA from '../InstallPWA/InstallPWA';
 
 import './Stock.scss';
 
 const Stock = () => {
     return (
         <>
+            <InstallPWA />
             <MobileMenu cn={'mobile-menu-stock'} />
             <div className="stock__img-wrap">
                 <img className="stock__img" src={ImgStock} alt="ImgStock" />
